00c68a8d7441d85ea59b7a560e2bc7e34e132d79
Teerapong Manakul <imumi@msn.com>
2016-07-22 01:34:20 +0700

    make auth

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Authentication Routes...
+Route::get('login', 'Auth\AuthController@showLoginForm');
+Route::post('login', 'Auth\AuthController@login');
+Route::get('logout', 'Auth\AuthController@logout');
+
+// Password Reset Routes...
+Route::get('password/reset/{token?}', 'Auth\PasswordController@showResetForm');
+Route::post('password/email', 'Auth\PasswordController@sendResetLinkEmail');
+Route::post('password/reset', 'Auth\PasswordController@reset');
+
+Route::get('/', 'HomeController@index');
