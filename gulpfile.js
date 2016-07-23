b829d211beb937a72ba1b04f2a7812ccd8740678
Teerapong Manakul <imumi@msn.com>
2016-07-23 20:34:11 +0700

    build font-awesome and ionicons with elixir

--- gulpfile.js
+++ gulpfile.js
@@ -14,16 +14,20 @@ var elixir = require('laravel-elixir');
 elixir(function(mix) {
     mix
     .styles([
-        'bootstrap/css/bootstrap.css',
-        'dist/css/AdminLTE.css',
-        'dist/css/skins/skin-blue.css'
-    ], 'public/css/all.css', 'node_modules/admin-lte')
+        'admin-lte/bootstrap/css/bootstrap.css',
+        'admin-lte/dist/css/AdminLTE.css',
+        'admin-lte/dist/css/skins/skin-blue.css',
+        'font-awesome/css/font-awesome.css',
+        'ionicons/dist/css/ionicons.css'
+    ], 'public/css/all.css', 'node_modules')
+    .copy('node_modules/font-awesome/fonts', 'public/build/fonts')
+    .copy('node_modules/ionicons/dist/fonts', 'public/build/fonts')
 
     .scripts([
-        'plugins/jQuery/jquery-2.2.3.min.js',
-        'bootstrap/js/bootstrap.min.js',
-        'dist/js/app.min.js'
-    ], 'public/js/all.js', 'node_modules/admin-lte')
+        'admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
+        'admin-lte/bootstrap/js/bootstrap.min.js',
+        'admin-lte/dist/js/app.min.js'
+    ], 'public/js/all.js', 'node_modules')
 
     .version(['public/css/all.css', 'public/js/all.js'])
     ;
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,6 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>AdminLTE 2 | Starter</title>
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">  <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
   <link rel="stylesheet" type="text/css" href="{{ url(elixir("css/all.css")) }}">
   <!--[if lt IE 9]>
   <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
