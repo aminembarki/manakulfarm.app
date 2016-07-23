15fb31e498ecf274babb97a9a2f74048949c5656
Teerapong Manakul <imumi@msn.com>
2016-07-23 20:18:23 +0700

    add admin-alt template

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,19 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix
+    .styles([
+        'bootstrap/css/bootstrap.css',
+        'dist/css/AdminLTE.css',
+        'dist/css/skins/skin-blue.css'
+    ], 'public/css/all.css', 'node_modules/admin-lte')
+
+    .scripts([
+        'plugins/jQuery/jquery-2.2.3.min.js',
+        'bootstrap/js/bootstrap.min.js',
+        'dist/js/app.min.js'
+    ], 'public/js/all.js', 'node_modules/admin-lte')
+
+    .version(['public/css/all.css', 'public/js/all.js'])
+    ;
 });
